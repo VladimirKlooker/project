ddd55894f78306a2a22b699109931006d200ce5f
Vladimir <klookerby@gmail.com>
2021-11-15 22:21:10 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// alert('Hello world');
+
+// const result = confirm('are yore here?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?', '18');
+// console.log(answer +5);
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
+
+console.log(typeof(null));
+
+
